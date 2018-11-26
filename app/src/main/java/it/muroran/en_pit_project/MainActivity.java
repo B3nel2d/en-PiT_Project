c92ef8fa46cba782db4eb569f21e9b07b3985443
BlackHoodie <blackhoodie416@gmail.com>
2018-11-26 15:24:53 +0900

    Created MainActivity as default

--- app/src/main/java/it/muroran/en_pit_project/MainActivity.java
+++ app/src/main/java/it/muroran/en_pit_project/MainActivity.java
@@ -1,9 +1,13 @@
 package it.muroran.en_pit_project;
 
-//import
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
-public class MainActivity{
+public class MainActivity extends AppCompatActivity {
 
-
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 }
