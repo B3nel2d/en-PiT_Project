ab53b18596b15aca865b0b2af188fdb7d6362e65
yuyakoizumi <44692397+yuyakoizumi@users.noreply.github.com>
2018-12-03 14:44:02 +0900

    home
    2 bottons are completed!!!!

--- app/src/main/java/it/muroran/en_pit_project/MainActivity.java
+++ app/src/main/java/it/muroran/en_pit_project/MainActivity.java
@@ -5,9 +5,13 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    // b1(ずかん)が押された時の処理
+   // public void b1(View v)
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
