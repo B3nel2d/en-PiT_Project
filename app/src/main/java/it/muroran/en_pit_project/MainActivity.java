f0d3fb70ebc126b9ed67b1673f79771373cfd4e6
yuyakoizumi <44692397+yuyakoizumi@users.noreply.github.com>
2018-12-05 16:43:55 +0900

    さつえい、ずかんともに画面遷移可能

--- app/src/main/java/it/muroran/en_pit_project/MainActivity.java
+++ app/src/main/java/it/muroran/en_pit_project/MainActivity.java
@@ -8,31 +8,23 @@ import android.widget.Button;//ボタン利用のため追加
 
 
 public class MainActivity extends AppCompatActivity {
-    private Button s_btn;//撮影用ボタン
 
-    // ずかんが押された時の処理
-    public void zukanTapped(View view) {
-        Intent intent = new Intent(this, zukanActivity.class);
+    // さつえいがタップされた時の処理
+    public void satsueiTapped(View view) {
+        Intent intent = new Intent(this, CameraCaptureActivity.class);
         startActivityForResult(intent,0);
     }
+
+    // ずかんがタップされた時の処理
+    public void zukanTapped(View view) {
+        Intent intent = new Intent(this, zukanActivity.class);
+        startActivityForResult(intent, 0);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        s_btn = (Button) findViewById(R.id.b0);
-
-
-        s_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // インテントへのインスタンス生成
-                Intent intent = new Intent(MainActivity.this, CameraCaptureActivity.class);
-                //                // サブ画面の呼び出し
-                startActivity(intent);
-            }
-        });
-
     }
 }
 
