3ebcde1b1f2640f3ba500fe557e7d89b47f89879
yuyakoizumi <44692397+yuyakoizumi@users.noreply.github.com>
2018-12-05 14:08:45 +0900

    前回やった結果
    zukanActivityつくったよ

--- app/src/main/java/it/muroran/en_pit_project/MainActivity.java
+++ app/src/main/java/it/muroran/en_pit_project/MainActivity.java
@@ -1,17 +1,31 @@
 package it.muroran.en_pit_project;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.view.View.OnClickListener;
 
 public class MainActivity extends AppCompatActivity {
 
-    // b1(ずかん)が押された時の処理
-   // public void b1(View v)
-    
+    private Button b1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        b1=(Button)findViewById(R.id.b1);
+        b1.setOnClickListener((OnClickListener) this);
+    }
+
+    // b1(ずかん)が押された時の処理
+    public void onClick(View v){
+        if(v==b1){
+            Intent intent = new Intent(this, zukanActivity.class);
+            startActivityForResult(intent,0);
+        }
     }
 
 }
---
+++ app/src/main/java/it/muroran/en_pit_project/zukanActivity.java
@@ -0,0 +1,13 @@
+package it.muroran.en_pit_project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class zukanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zukan);
+    }
+}
