cf5f849c1d4fdb306a919fd21b73497df4b675c7
yuyakoizumi <44692397+yuyakoizumi@users.noreply.github.com>
2018-12-05 14:50:53 +0900

    ずかんひらくようになたよ
    onClickをonButtonTappedにした

--- app/src/main/java/it/muroran/en_pit_project/MainActivity.java
+++ app/src/main/java/it/muroran/en_pit_project/MainActivity.java
@@ -4,28 +4,18 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.view.View.OnClickListener;
 
 public class MainActivity extends AppCompatActivity {
-
-    private Button b1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        b1=(Button)findViewById(R.id.b1);
-        b1.setOnClickListener((OnClickListener) this);
     }
 
-    // b1(ずかん)が押された時の処理
-    public void onClick(View v){
-        if(v==b1){
+    // ずかんが押された時の処理
+    public void zukanTapped(View view){
             Intent intent = new Intent(this, zukanActivity.class);
             startActivityForResult(intent,0);
         }
     }
-
-}
