e3fbb50d1403c43293e3ac95aea4e445fe613d1e
BlackHoodie <blackhoodie416@gmail.com>
2019-02-07 18:21:38 +0900

    Revert "Merge branch 'master' of https://github.com/BlackHoodieDog/en-PiT_Project"
    This reverts commit 165ceda0765573f18aec11b4b7993536fddfd7fd, reversing
    changes made to e1cfbc43872eb7405cfa2d2f7d5730c1559ef4b9.

--- app/src/main/java/it/muroran/en_pit_project/CameraCaptureActivity.java
+++ app/src/main/java/it/muroran/en_pit_project/CameraCaptureActivity.java
@@ -21,6 +21,7 @@ import android.view.SurfaceView;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class CameraCaptureActivity extends android.app.Activity
@@ -172,10 +173,6 @@ public class CameraCaptureActivity extends android.app.Activity
                                 contentName = "タラバガニ";
                                 break;
                         }
-            for (int index = 0; index < AppManager.getInstance().fishes.size(); index ++) {
-                if(AppManager.getInstance().fishes.get(index).getId().equals(result.getText())){
-                    if(!AppManager.getInstance().fishes.get(index).isCaptured()){
-                        AppManager.getInstance().fishes.get(index).setCaptured(true);
                     }
                 }
             }
